fa682bc009ca265fac10099bdd7ea659b33dcd1f
fristyy <31688915+fristyy@users.noreply.github.com>
2019-01-03 15:26:51 +0800

    Add files via upload

---
+++ zfl/zfl/pipelines.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.pipelines.images import ImagesPipeline
+from scrapy.exceptions import DropItem
+from scrapy.http import Request
+import re
+
+class ZflPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        for img_url in item['imgurl']:
+            yield Request(img_url, meta={'item':item['title']})
+    
+    def file_path(self, request, response=None, info=None):
+        img_alt = request.url.split('/')[-1]
+        title = request.meta['item']
+        title = re.sub(r'[？\\*|“<>:/]', '', title)
+        filename = u'full{},{}'.format(title, img_alt)
+        return filename
+    
+    def item_completed(self, results, item, info):
+        image_path = [x['path'] for ok, x in results if ok]
+        if not image_path:
+            raise DropItem('Item contains no images')
+        item['image_paths'] = image_path
+        return item    
---
+++ zfl/zfl/spiders/zfl_spider.py
@@ -0,0 +1,34 @@
+import scrapy
+from zfl.items import ZflItem
+
+class zfl(scrapy.Spider):
+
+	name = 'zflspider'
+	allowed_domains = ['952lls.com']
+	start_urls = ['https://952lls.com/MiiTao/']
+	
+	def parse(self, response):
+		hreflist = response.xpath('//h2//@href').extract()
+		for href in hreflist:
+			href = response.urljoin(href)
+			yield scrapy.Request(href, callback=self.content)
+			
+		next_url = response.xpath('//li[@class="next-page"]/a/@href').extract_first()
+		if next_url is not None:
+			yield response.follow(next_url, callback = self.parse)
+	
+		
+	def content(self, response): 
+		item = ZflItem()	
+		title = response.xpath('//h1/text()').extract_first()
+		item['title']=title
+		item['imgurl']=response.xpath('//p/img/@src').extract()
+		yield item	
+		next_url = response.xpath('//li[@class="next-page"]/a/@href').extract_first()
+		if next_url is not None:
+			next_url = response.urljoin(next_url)
+			yield response.follow(next_url, callback=self.content)
+		
+		
+			
+		
